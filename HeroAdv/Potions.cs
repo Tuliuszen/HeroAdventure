97361592cc30e62853fcf093523d4b09eb6f94c0
Tuliusz <60552476+Tuliuszen@users.noreply.github.com>
2020-07-13 21:07:14 +0200

    Add files via upload

---
+++ HeroAdv/Potions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroAdventure
+{
+    class Potions:Items
+    {
+        public Potions(string itemName, int itemHealth, int itemStamina, int itemCost)
+        {
+            this.itemName = itemName;
+            this.itemHealth = itemHealth;
+            this.itemStamina = itemStamina;
+            this.itemCost = itemCost;
+        }
+    }
+}
